b66684723fb59d152eb3eb1b9a7c789de498a876
[Andrew Core] <[adcore89@gmail.com]>
2021-03-24 07:17:24 -0400

    formSubmitHandler doesn work

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -1,14 +1,32 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerE1 = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    if (username) {
+        getUserRepos(username);
+        nameInputE1.value= "";
+    } else{
+        alert("please enter a GitHub username");
+    }
+};
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
-getUserRepos("fraudwheeldrive");
 
-
-
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+    
+userFormEl.addEventListener( "submit", formSubmitHandler);  
