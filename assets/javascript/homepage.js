534383d0b3c3936bc4213c4ba95d3f533c089187
[Andrew Core] <[adcore89@gmail.com]>
2021-03-30 21:22:59 -0400

    chapter 5 done

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsE1 =document.querySelector("#language-buttons", "<div>");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -90,5 +91,25 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data){
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert(" Error: " + response.statusText);
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+}
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsE1.addEventListener("click",buttonClickHandler);
